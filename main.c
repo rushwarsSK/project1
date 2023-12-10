e25a19789d19e8789cbaa4447ec9cd476f65d6ff
RUSHER <rushwarssk@gmail.com>
2023-12-10 16:55:32 +0100

    Added getMax() to main.c

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+int getMax(int n1, int n2);
+
 int main() {
     printf("Nieco\n");
+    printf("max(67, 5) = %d\n", getMax(67, 5));
 
     return 0;
+}
+
+int getMax(int n1, int n2) {
+    return (n1 > n2) ? n1 : n2;
 }
